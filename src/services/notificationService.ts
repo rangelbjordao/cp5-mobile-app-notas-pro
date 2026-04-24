d63b9dd28950a7541c66c3c34302c4403b974cdc
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-04-24 14:48:24 -0300

    Update notificationService.ts

--- src/services/notificationService.ts
+++ src/services/notificationService.ts
@@ -12,6 +12,10 @@ Notifications.setNotificationHandler({
 
 export const enviarNotificacaoLocal = async (titulo: string, corpo: string) => {
   try {
+    const permitido = await solicitarPermissaoNotificacao();
+
+    if (!permitido) return;
+
     await Notifications.scheduleNotificationAsync({
       content: {
         title: titulo,
@@ -22,4 +26,20 @@ export const enviarNotificacaoLocal = async (titulo: string, corpo: string) => {
   } catch (error) {
     console.log("Erro ao disparar notificação local:", error);
   }
+};
+
+export const solicitarPermissaoNotificacao = async () => {
+  const { status } = await Notifications.getPermissionsAsync();
+
+  if (status !== "granted") {
+    const { status: novoStatus } =
+      await Notifications.requestPermissionsAsync();
+
+    if (novoStatus !== "granted") {
+      console.log("Permissão de notificação negada");
+      return false;
+    }
+  }
+
+  return true;
 };
